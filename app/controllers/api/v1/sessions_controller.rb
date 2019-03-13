85ff4b3f9654e8db8dc72bb6fecdd0bdc7c39fb8
ascbr <adilio.scbr@gmail.com>
2019-03-13 15:30:17 -0600

    REST API, PERFIL

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,28 +9,34 @@ module API
         @user = User.find_by(email: params[:email])
         
         if @user.nil?
-          head(:unathorized)
-         
-        else
-          if @user.valid_password?(params[:password])
+          render json: {message: 'user not found'}, status: :unathorized
+        elsif @user.valid_password?(params[:password])
             jwt = JWT.encode(
-              { user_id: @user.id, user_email: @user.email },
+              { user_id: @user.id, user_email: @user.email, exp: (Time.now + 2.week).to_i },
               KEY,
               'HS256'
             )
             request.headers[:token] = jwt
-            render json: { status: 'OK', message: 'Valid credenttials', data: jwt }
-          end
+            render json: { status: 'OK', message: 'Valid credenttials', data: jwt }, status: :ok
+        else
+            render json: {message: 'invalid credentials'}, status: :unathorized
         end
       end
 
       def index
+        puts KEY
         data = request.headers[:token]
-        decoded_token = JWT.decode data, nil, false
-        user_id = decoded_token[0]['user_id']
-
-        @user = User.find(user_id)
-        render json: {  user: @user }
+        begin
+          decoded_token = JWT.decode data, KEY, true, { algorithm: 'HS256'}
+          user_id = decoded_token[0]['user_id']
+          @user = User.find(user_id)
+          render json: { user: @user }
+        rescue JWT::VerificationError
+          render json: { info: 'token expired' }, status: :unathorized
+        rescue
+          render json: { info: 'user not found' }, status: :not_found
+        end
+        
       end
 
     end
